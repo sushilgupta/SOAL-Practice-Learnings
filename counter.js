dd4871a6e50376b4944397760adaeeb1faa6a64e
codewithsushil <sushilgpt2@gmail.com>
2021-05-24 17:55:03 +0530

    Adding setInterval to automate count

--- counter.js
+++ counter.js
@@ -4,11 +4,13 @@ function count() {
     counter = counter + 1;
     document.querySelector('h1').innerHTML = counter;
 
-    if (counter % 10 === 0) {
-        alert(`You have counted till ${counter}`);
-    }
+    // if (counter % 10 === 0) {
+    //     alert(`You have counted till ${counter}`);
+    // }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
    document.querySelector('button').onclick = count;
+
+   setInterval(count, 1000);
 })
