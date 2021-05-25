45c7c201bc037336eb5069286b979777a3df42b1
codewithsushil <sushilgpt2@gmail.com>
2021-05-25 19:16:54 +0530

    Objects in JS

---
+++ object.js
@@ -0,0 +1,14 @@
+let person = {
+    first: 'Sushil',
+    last: 'Gupta',
+    age: 29
+};
+
+console.log(person);
+
+person ["first"];
+person.first
+person.age
+
+person ["age"];
+person["age" , ["first"];]
