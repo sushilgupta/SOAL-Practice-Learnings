39014a9a2dd26f5ac2cc1a83b767db1d7ed78044
codewithsushil <sushilgpt2@gmail.com>
2021-05-30 00:00:58 +0530

    Increasing and decreasing value of a variable, add, substract,/,*

---
+++ basic-javascript.js
@@ -0,0 +1,22 @@
+let myName;
+myName = "Sushil";
+
+// We don't use quotations to a number while declaring a value to a variable
+let myAge = 29;
+
+let details = ["Sushil", 29, "Gupta"];
+details;
+
+
+var sum = 1+1;
+var product = 5 * 4;
+var quotient = 10/2;
+
+let myVar = 15;
+myVar++;
+//Increment a value of variable by adding value 1
+
+let myVar = 10;
+myVar--;
+//Decreasing value of a variable by 1
+
