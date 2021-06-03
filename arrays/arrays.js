a08a08a393ddf51b339bfee15afd97936511c583
codewithsushil <sushilgpt2@gmail.com>
2021-06-03 22:48:17 +0530

    Adding arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,7 @@
+//Collection of arrays..
+let emp = ["Sushil", "Gupta", 29];
+//console.log(emp);
+
+//storing value to a new variable from existing arrays
+let myArray = ['Sushil', 'Guru'];
+let newArray = myArray[1];
