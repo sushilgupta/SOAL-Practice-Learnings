4298566ad3f56e93d6748e2d0dac5d36b786297e
codewithsushil <sushilgpt2@gmail.com>
2021-06-04 01:20:04 +0530

    Removing 0th index data using .shift() and replacing it with new data using .unshift()

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -10,6 +10,16 @@ let newArray = myArray[1];
 let myArray1 = ['Sushil', 'Guru'];
 myArray1.push('Kadir');
 
-//Removing data from Array using pop()
+//Removing last data from Array using .pop()
 var myArray2 = [["Sushil", 29], ["Guru", 26]];
 var popMyArray2 = myArray2.pop();
+
+//Removing first data from Array using .shift()
+var myArray3 = [["Guru", "Nath"], ["Kadir", "Hussain"]];
+var shifyMyArray3 = myArray3.shift();
+
+//Removing first data from Array using .shift() and adding data to the start using .unshift();
+var myArray5 = [["Guru", "Nath"], ["Kadir", "Hussain"]];
+var myArray5.shift();
+var myArray5.unshift(["Manisha", "Jha"]);
+
